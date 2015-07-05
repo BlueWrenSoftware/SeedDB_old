1bff6d74122755b4979273e51374b18fceac16eb
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-07-05 17:10:06 +1000

    Updated seedlist with new look.

--- frontend/js/seedlistCtrl.js
+++ frontend/js/seedlistCtrl.js
@@ -1,9 +1,18 @@
 (function() {
-	angular.module('seed-db').controller('seedlistCtrl', function ($scope, $http) {
-		$scope.vm = {};
-		$http.get('/api/seedlist').success(function(data) {
-			console.log(data);
-			$scope.vm.seedArray = data;
-		});
-	});
+    angular.module('seed-db').controller('seedlistCtrl', ['$scope', 'seedlist', 'seedtypes',  function ($scope, seedlist, seedtypes) {
+
+	$scope.vm = {}
+	$scope.vm.seedArray = seedlist.data;
+	$scope.vm.seedTypes = seedtypes.data;
+	console.log($scope.vm.seedTypes);
+	console.log($scope.vm.seedArray);
+	
+	for (var i = 0; i < $scope.vm.seedArray.length; i++) {
+	    $scope.vm.seedArray[i].isEditing = false;
+	};
+
+	$scope.vm.toggleEdit = function(seedIndex) {
+	    $scope.vm.seedArray[seedIndex].isEditing = !$scope.vm.seedArray[seedIndex].isEditing;
+	};
+    }]);
 })();
