de1e07097583bd2e24552e0ed19726b40ad9883a
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-07-09 20:03:29 +1000

    Updated states.js

--- frontend/js/states.js
+++ frontend/js/states.js
@@ -1,36 +1,32 @@
 (function() {
-    angular
-	.module('seed-db')
-	.config(['$stateProvider',
-        	 function($stateProvider) {
-          	     $stateProvider
-            		 .state('seedlist', {
-	      		     url: '',
-              		     templateUrl: 'templates/seedlist.html',
-			     controller: 'seedlistCtrl as seedlistCtrl',
-			     resolve: {
-				 seedlist: function(GetDataService) {
-				     return GetDataService.getSeeds();
-				 },
-				 seedtypes: function(GetDataService) {
-				     return GetDataService.getSeedTypes();
-				 }
-			     }
-			     
-			 })
-			 .state('packetlist', {
-			     url: '/packetlist/{seedId}',
-			     templateUrl: 'templates/packetlist.html',
-			     controller: 'packetlistCtrl as controller',
-			     resolve: {
-				 packets: function (GetDataService) {
-				     return GetDataService.getPackets();
-				 },
-				 companies: function (GetDataService) {
-				     return GetDataService.getCompanies();
-				 }
-			     }
-			 });
-		 }
-		])
+    angular.module('seed-db').config(['$stateProvider', function($stateProvider) {
+        $stateProvider
+            .state('seedlist', {
+	      	url: '',
+              	templateUrl: 'templates/seedlist.html',
+		controller: 'seedlistCtrl as seedlistCtrl',
+		resolve: {
+		    seedlist: function(GetDataService) {
+			return GetDataService.getSeeds();
+		    },
+		    seedtypes: function(GetDataService) {
+			return GetDataService.getSeedTypes();
+		    }
+		}
+		
+	    })
+	    .state('packetlist', {
+		url: '/packetlist/{seedId}',
+		templateUrl: 'templates/packetlist.html',
+		controller: 'packetlistCtrl as controller',
+		resolve: {
+		    packets: function (GetDataService) {
+			return GetDataService.getPackets();
+		    },
+		    companies: function (GetDataService) {
+			return GetDataService.getCompanies();
+		    }
+		}
+	    });
+    }]);
 })()
