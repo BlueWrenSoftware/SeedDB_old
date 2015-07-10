fa26d78d21a78e1c4c48ae975364e156108870ad
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-07-10 22:18:46 +1000

    Packet dates are now formatted as dates.

--- frontend/model/Packet.js
+++ frontend/model/Packet.js
@@ -16,8 +16,8 @@
 		    companyUrl: data.companyUrl
 		});
 	    }
-	    this._datePurchased = data.datePurchased;
-	    this._dateUseBy = data.dateUseBy;
+	    this._datePurchased = new Date(data.datePurchased);
+	    this._dateUseBy = new Date(data.dateUseBy);
 	    this._seedCount = data.seedCount;
 	    this._packetTreatment = data.packetTreatment;
 	    this._storageLocation = data.storageLocation;
@@ -59,7 +59,7 @@
 	Object.defineProperty(Packet.prototype,
 			      'datePurchased',
 			      {
-				  get: function () { return this._datePurchased; },
+				  get: function () { return this._datePurchased.toDateString(); },
 				  set: function (value) {
 				      this._datePurchased = value;
 				      if (this._isAutoSave) {
@@ -72,7 +72,7 @@
 	Object.defineProperty(Packet.prototype,
 			      'dateUseBy',
 			      {
-				  get: function () { return this._dateUseBy; },
+				  get: function () { return this._dateUseBy.toDateString(); },
 				  set: function (value) {
 				      this._dateUseBy = value;
 				      if (this._isAutoSave) {
@@ -121,26 +121,26 @@
 
 	Packet.prototype.toRaw = function () {
 	    return {
-		packetId: this.packetId,
-		seedId: this.seedId,
-		companyId: this.company.companyId,
-		datePurchased: this.datePurchased,
-		dateUseBy: this.dateUseBy,
-		seedCount: this.seedCount,
-		packageTreatment: this.packageTreatment,
-		storageLocation: this.storageLocation
+		packetId: this._packetId,
+		seedId: this._seedId,
+		companyId: this._company._companyId,
+		datePurchased: this._datePurchased,
+		dateUseBy: this._dateUseBy,
+		seedCount: this._seedCount,
+		packageTreatment: this._packageTreatment,
+		storageLocation: this._storageLocation
 	    }
 	}
 
 	Packet.prototype.save = function () {
-	    if (this.packet.packetId !== null) {
+	    if (this.packetId !== null) {
 		throw "Attempted to save a non-existent packet."
 	    }
 	    PostDataService.createOrUpdatePacket(this.toRaw());
 	}
 
 	Packet.prototype.create = function () {
-	    if (this.packet.packedId === null) {
+	    if (this.packedId === null) {
 		throw "Attempted to create an existing packet."
 	    }
 	    PostDataService.createOrUpdatePacket(this.toRaw());
