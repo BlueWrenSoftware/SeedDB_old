2852fef2accf2c50a2cabe5874353b74993a7a5d
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-07-11 20:31:18 +1000

    PacketListController now can create new packets.

--- frontend/js/packetlistCtrl.js
+++ frontend/js/packetlistCtrl.js
@@ -1,8 +1,21 @@
 (function() {
-    angular.module('seed-db').controller('packetlistCtrl', ['$scope', 'packets', 'companies', function ($scope, packets, companies) {
+    angular.module('seed-db').controller('packetlistCtrl', ['$scope', 'packets', 'companies', 'seedId', 'Packet', 'Constants', function ($scope, packets, companies, seedId, Packet, Constants) {
+	var newPacketData = {
+	    packetId: null,
+	    packetCode: null,
+	    seedId: seedId,
+	    company: companies[0],
+	    datePurchased: new Date() / Constants.MILLISECONDS_IN_DAY,
+	    dateUseBy: new Date() / Constants.MILLISECONDS_IN_DAY,
+	    seedCount: 0,
+	    packetTreatment: null,
+	    storageLocation: null
+	};
+
 	$scope.vm = {
 	    packets: packets,
-	    companies: companies
+	    companies: companies,
+	    newPacket: new Packet(newPacketData)
 	};
 
 	$scope.view = {
@@ -13,5 +26,16 @@
 	    editSeedCount: _.fill(new Array($scope.vm.packets.length), false),
 	    editStorageLocation: _.fill(new Array($scope.vm.packets.length), false)
 	};
+
+	$scope.controls = {
+	    addNewPacket: function() {
+		$scope.vm.newPacket.create().then(function (packetId) {
+		    $scope.vm.newPacket._packetId = packetId;
+		    $scope.vm.packets.push($scope.vm.newPacket);
+		    $scope.vm.newPacket = new Packet(newPacketData);
+		});
+	    }
+	};
+						 
     }]);
 })();
