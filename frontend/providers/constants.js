488798df94ac69b870bcc6674d0fc6515623b599
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-07-11 18:44:24 +1000

    Renamed constants.js to Constants.js

--- frontend/providers/constants.js
+++ frontend/providers/constants.js
@@ -1,5 +1,5 @@
 (function () {
-    angular.module('seed-db').factory('constants', function () {
+    angular.module('seed-db').factory('Constants', function () {
 	var constants = {
 	    MILLISECONDS_IN_DAY: 24 * 60 * 60 * 1000
 	};
