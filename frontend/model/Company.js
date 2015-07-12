c35fa4c9b7ee33007c2912634af066756c03ff90
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2015-07-12 19:37:26 +1000

    Company objects now have a create and toRaw method.

--- frontend/model/Company.js
+++ frontend/model/Company.js
@@ -1,5 +1,5 @@
 (function() {
-    angular.module('seed-db').factory('Company', function() {
+    angular.module('seed-db').factory('Company', ['PostDataService', function(PostDataService) {
 	var Company = function(data) {
 	    this._companyId = data.companyId;
 	    this._companyName = data.companyName;
@@ -8,12 +8,31 @@
 	};
 	
 	Object.defineProperty(Company.prototype, 'companyId', { get: function () {return this._companyId;} });
-	Object.defineProperty(Company.prototype, 'companyName', { get: function () {return this._companyName;} });
+	Object.defineProperty(Company.prototype, 'companyName', {
+	    get: function () {return this._companyName;},
+	    set: function (value) {this._companyName = value;}
+	});
 	Object.defineProperty(Company.prototype, 'companyAddress', { get: function () {return this._companyAddress;} });
 	Object.defineProperty(Company.prototype, 'companyUrl', { get: function () {return this._companyUrl;} });
+
+	Company.prototype.toRaw = function () {
+	    return {
+		companyId: this._companyId,
+		companyName: this._companyName,
+		companyAddress: this._companyAddress,
+		companyUrl: this._companyUrl
+	    };
+	};
+
+	Company.prototype.create = function() {
+	    if (this.companyId !== null) {
+		throw "Attempted to create an existing company."
+	    }
+	    return PostDataService.createOrUpdateCompany(this.toRaw());
+	};
 	
 	return Company;
 	
-    });
+    }]);
 })();
 
