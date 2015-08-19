d030cd5932a89c86ed525616947dce5a5f59bfb3
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2015-08-19 13:06:23 +1000

    Updated constants.js

--- frontend/providers/constants.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    angular.module('seed-db').factory('Constants', function () {
-	var constants = {
-	    MILLISECONDS_IN_DAY: 24 * 60 * 60 * 1000
-	};
-	return constants;
-    });
-})();
